2b9ce5be3f76dc8d6dec84984526eb514be79892
Victor Caruso <vic.caruso@gmail.com>
2022-05-13 17:04:53 -0700

    Add item count to header and a button to clear list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useList } from '../context/ListContext';
 
 export default function Header() {
+  const { listState } = useList();
+
   return (
     <header>
       <h1>Shopping List</h1>
+      <div>
+        <p>{`You have ${listState.length} items in your shopping list.`}</p>
+        <button>Clear list</button>
+      </div>
     </header>
   );
 }
